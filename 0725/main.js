e4ec37170a77d424b36507f2ba5ba86a08fdd2d1
MO <48742497+msh9900@users.noreply.github.com>
2022-07-25 19:01:55 +0900

    7월25일 과제

---
+++ 0725/main.js
@@ -0,0 +1,13 @@
+// const user = {
+//      id: 1,
+//      name: "tetz",
+//      email: ["xenosign@naver.com", "tetz@spreatics.com"],
+//     };
+//     const str = JSON.stringify(user);
+//     console.log(str);
+//     console.log(typeof str);
+//     const obj = JSON.parse(str);
+//     console.log(obj);
+//     console.log(typeof obj);
+import myData from "./myData.json" assert { type: "json" };
+console.log(myData);
