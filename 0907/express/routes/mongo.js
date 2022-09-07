d6d4a64bf7fd5842e37394e23b77baf012188723
MO <48742497+msh9900@users.noreply.github.com>
2022-09-07 18:53:54 +0900

    9월 7일 공부

---
+++ 0907/express/routes/mongo.js
@@ -0,0 +1,35 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =
+  'mongodb+srv://root:root@cluster0.rqpf5kr.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function main() {
+  await client.connect();
+  const users = client.db('kdt1').collection('users');
+  await users.deleteMany({});
+  await users.insertMany([
+    {
+      name: 'pororo',
+      age: 5,
+    },
+    {
+      name: 'loopy',
+      age: 6,
+    },
+    {
+      name: 'crong',
+      age: 4,
+    },
+  ]);
+  const data = users.find({});
+  const arr = await data.toArray();
+  console.log(data);
+  console.log(arr);
+
+  await client.close();
+}
+
+main();
