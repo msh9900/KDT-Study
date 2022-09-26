a1c3c60462fe15b0a5774f8b30ca45cf164746fc
MO <48742497+msh9900@users.noreply.github.com>
2022-09-26 18:11:26 +0900

    09월 26일 리엑트 수업

---
+++ 0926/react/react-kdt/src/components/BtnToNaver.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class BtnToNaver extends Component {
+  render() {
+    return <a href="https://www.naver.com/">네이버 이동</a>;
+  }
+}
+// function BtnToNaver() {
+//   return <a href="https://www.naver.com/">네이버 이동</a>;
+// }
+export default BtnToNaver;
