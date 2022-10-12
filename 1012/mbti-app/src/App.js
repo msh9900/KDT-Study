ada41050f590ca3be7a52e953bd0697992fd5f4b
MO <48742497+msh9900@users.noreply.github.com>
2022-10-12 18:46:42 +0900

    10월 12일 mbti 페이지

---
+++ 1012/mbti-app/src/App.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import Start from './components/Start';
+import GlobalStyle from './components/GlobalStyle';
+import { useSelector } from 'react-redux';
+import Mbti from './components/Mbti';
+const Main = styled.main`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 500px;
+  padding: 0 35px;
+  margin: auto;
+  text-align: center;
+`;
+
+function App() {
+  const page = useSelector((state) => state.mbti.page);
+  return (
+    <>
+      <GlobalStyle />
+      <Main>{page === 0 ? <Start /> : <Mbti />}</Main>
+    </>
+  );
+}
+
+export default App;
